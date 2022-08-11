3a36aa5b0a8218345c96c0f6ab03977a73a5b03c
Javid-Git <cavid.alizade00@mail.ru>
2022-08-12 00:20:39 +0400

    about responsiveness finished

--- Front/script.js
+++ Front/script.js
@@ -196,5 +196,42 @@ $(document).ready(function () {
         $('.mobile-filter-big').toggleClass('show-mobile-filter')
         $('.mobile-sorting').toggleClass('mobile-sorting-top')
     })
+
+    $('.about4-items').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 })
 

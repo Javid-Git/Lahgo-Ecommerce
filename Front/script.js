2e6d1ebac4f19be1948abfa813db2c240c3dca7f
Javid-Git <cavid.alizade00@mail.ru>
2022-08-13 04:23:23 +0400

    detail page finished

--- Front/script.js
+++ Front/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $(window).on('scroll', function () {
         if ($(this).scrollTop() > 10) {
-            $('#center-header').addClass('center-header-sticky');   
-            $('.sidebar').addClass('sidebar-long');   
-            
+            $('#center-header').addClass('center-header-sticky');
+            $('.sidebar').addClass('sidebar-long');
+
         } else {
             $('#center-header').removeClass('center-header-sticky');
-            $('.sidebar').removeClass('sidebar-long');   
+            $('.sidebar').removeClass('sidebar-long');
 
         }
     });
@@ -130,37 +130,37 @@ $(document).ready(function () {
             // instead of a settings object
         ]
     });
-    $(document).on('click', '.slider-button-next', function(){
+    $(document).on('click', '.slider-button-next', function () {
         $('.slider-button-prev').addClass('show-slider-button');
         $('.slider-button-next').addClass('hide-slider-button');
     })
-    $(document).on('click', '.slider-button-prev', function(){
+    $(document).on('click', '.slider-button-prev', function () {
         $('.slider-button-next').removeClass('hide-slider-button');
         $('.slider-button-prev').removeClass('show-slider-button');
 
     })
-    $(document).on('click', '.dropdowns', function(e){
+    $(document).on('click', '.dropdowns', function (e) {
         console.log($(this).children('.lists').toggleClass('dropdowns-visible'))
     })
-    $(document).on('click', '.minicart-button', function(e){
+    $(document).on('click', '.minicart-button', function (e) {
         e.preventDefault();
         $('#minicart').toggleClass('minicart-visible')
         $('.minicart-wraper').toggleClass('minicart-visible')
 
     })
-    $(document).on('click', ('#minicart-close', '.minicart-wraper'), function(){
+    $(document).on('click', ('#minicart-close', '.minicart-wraper'), function () {
         $('#minicart').toggleClass('minicart-visible')
         $('.minicart-wraper').toggleClass('minicart-visible')
     })
-    $(document).on('click', '#minicart-close', function(){
+    $(document).on('click', '#minicart-close', function () {
         $('#minicart').toggleClass('minicart-visible')
         $('.minicart-wraper').toggleClass('minicart-visible')
     })
-    $(document).on('click', '#mobile-menu-trigger', function(){
+    $(document).on('click', '#mobile-menu-trigger', function () {
         $('.sidebar').toggleClass('show-sidebar')
         $('.sidebar-wraper').toggleClass('show-sidebar')
     })
-    $(document).on('click','.sidebar-wraper', function(){
+    $(document).on('click', '.sidebar-wraper', function () {
         $('.sidebar').toggleClass('show-sidebar')
         $('.sidebar-wraper').toggleClass('show-sidebar')
     })
@@ -185,14 +185,14 @@ $(document).ready(function () {
         }
         $(this).prev().val(inputCount);
     })
-    $(document).on('click','#mobile-sort', function(){
+    $(document).on('click', '#mobile-sort', function () {
         $('.mobile-sort-list').toggleClass('show-mobile-sort')
     })
-    $(document).on('click','#mobile-filter', function(){
+    $(document).on('click', '#mobile-filter', function () {
         $('.mobile-filter-big').toggleClass('show-mobile-filter')
         $('.mobile-sorting').toggleClass('mobile-sorting-top')
     })
-    $(document).on('click','#cancel-filtering', function(){
+    $(document).on('click', '#cancel-filtering', function () {
         $('.mobile-filter-big').toggleClass('show-mobile-filter')
         $('.mobile-sorting').toggleClass('mobile-sorting-top')
     })
@@ -212,6 +212,96 @@ $(document).ready(function () {
                     slidesToScroll: 3,
                     infinite: true,
                     dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $(document).on('click', '.detail-size-container', function (e) {
+        let size = $(this).attr('value');
+        console.log(size);
+        $('.selected-size').empty();
+        $('.selected-size').append(size);
+    })
+
+    $(document).on('click', '.small-colors-optional', function (e) {
+        let size = $(this).attr('value');
+        console.log(size);
+        $('.selected-color').empty();
+        $('.selected-color').append(size);
+    })
+
+    $(document).on('click', '.small-colors-optional', function (e) {
+        $('.small-colors-optional').removeClass('active-selection');
+        $(this).addClass('active-selection');
+    })
+
+    $(document).on('click', '.detail-size-container', function (e) {
+        $('.detail-size-container').removeClass('active-selection');
+        $(this).addClass('active-selection');
+    })
+
+    $(document).on({
+        mouseenter: function () {
+            $('.tooltip-content').addClass('show-tooltip-content')
+        },
+        mouseleave: function () {
+            $('.tooltip-content').removeClass('show-tooltip-content')
+
+        }
+    }, ".svg-tooltip");
+
+    $(".slide-toggle").click(function (e) {
+        $(this).children('.toggle-list').slideToggle(400, function () {
+            // Animation complete.
+        });
+    });
+
+    $(document).on('click','.popup-pic', function(e){
+        let picsrc = $(this).children('img').attr('src');
+        $('.popup-wraper').css('background-image', 'url(' + picsrc + ')');
+    })
+
+    $(document).on('click', '.add-pic', function (e) {
+        let bcpic = $(this).children('img').attr('src')
+        $('.popup-wraper').css('background-image', 'url(' + bcpic + ')');
+        $('.popup-pic-list').toggle();
+    })
+    $(document).on('click', '#product-images-popup-close, .popup-modal', function () {
+        $('.popup-pic-list').toggle();
+    })
+
+    $('.detail1-left-mobile').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
                 }
             },
             {
